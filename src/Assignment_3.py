0b0761b7964cba705d276350b99d3c5bb8d9eb7c
LSWarss <ls.warss98@gmail.com>
2020-10-22 16:34:19 +0200

    Closing assignment 3 closes #1 closes #2

--- src/Assignment_3.py
+++ src/Assignment_3.py
@@ -2,3 +2,14 @@
 Napisz funkcj¦ wypisuj¡c¡ co drugi element listy w odwrotnej kolejno±ci. Mo»na
 u»y¢ metod wbudowanych dla list.
 """
+
+def reversedSecond(list):
+    list.reverse()
+    for i in range(len(list)): 
+        if i % 2 == 0:
+            print(list[i])
+
+
+testList = ["a","b","c","d","e"]
+
+reversedSecond(testList)

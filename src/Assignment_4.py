1465ac3ab381941b3d799073be869b9de7b2126f
LSWarss <ls.warss98@gmail.com>
2020-10-22 16:49:54 +0200

    Created and closed the merge assignment, but need to tweak it a little tomorrow Closes #3

--- src/Assignment_4.py
+++ src/Assignment_4.py
@@ -2,3 +2,51 @@
 Dane s¡ dwie listy. Napisz metod¦ Merge, która poa¡czy listy tak, »eby elementy
 pojawiaay si¦ na zmian¦.
 """
+
+import random
+
+# TODO: Correct this function or create new one while I've created this one, the way it only takes the longest list length opposite to just taking sum of the lengths
+
+def mergeOnSecond(listA, listB):
+    listC = []
+    if len(listA) > len(listB):
+        for i in range(len(listA)):
+            if i >= len(listB):
+                listC.append(listA)
+            else:
+                if i % 2 != 0:
+                    listC.append(listA[i])
+                else:
+                    listC.append(listB[i])
+    elif len(listA) < len(listB):
+        for i in range(len(listB)):
+            if i >= len(listA):
+                listC.append(listB)
+            else:
+                if i % 2 != 0:
+                    listC.append(listB[i])
+                else:
+                    listC.append(listA[i])
+    else:
+        randomBool = bool(random.randint(0,1))
+        if randomBool is True:
+            for i in range(len(listA)):
+                if i >= len(listB):
+                    listC.append(listA)
+                else:
+                    if i % 2 != 0:
+                        listC.append(listA[i])
+                    else:
+                        listC.append(listB[i])
+        if randomBool is False:
+            for i in range(len(listB)):
+                if i >= len(listA):
+                    listC.append(listB)
+                else:
+                    if i % 2 != 0:
+                        listC.append(listB[i])
+                    else:
+                        listC.append(listA[i])
+    return listC
+
+print(mergeOnSecond([1,2,3,4,5],[6,7,8,9,10]))

3004a6c8a0c8c9629443538f95fc168c77d993a0
LSWarss <ls.warss98@gmail.com>
2020-10-23 15:22:26 +0200

    Closes #4 Finished assignment 5

--- src/Assignment_5.py
+++ src/Assignment_5.py
@@ -4,3 +4,25 @@ na li±cie znajduj¡ si¦ liczby i aa«cuchy. Zakaadamy, »e wszystkie elementy 
 znajduj¡ si¦ na pocz¡tku, a aa«cuchy znajduj¡ si¦ w dalszej cz¦±ci listy - posortowane
 saownikowo.
 """
+
+def mixSort(array):
+    stringList = []
+    numbersList = []
+    for i in array:
+        if type(i) is str:
+            stringList.append(i)
+        else:
+            numbersList.append(i)
+    length = len(array)
+    stringList.sort()
+    numbersList.sort()
+    array.clear()
+
+    for i in range(length):
+        if i < len(numbersList):
+            array.append(numbersList[i])
+        else:
+            array.append(stringList[i - len(numbersList)])
+    return array
+
+print(mixSort(["lal", "bala",1,3,4,5,7,"Mara","dola",True]))

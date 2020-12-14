aa642d3691a6354d17a07a7ad04015169a57c9d5
LSWarss <ls.warss98@gmail.com>
2020-12-14 19:02:33 +0100

    Implementing assignmen 9.1

--- src/Assignment_4_1.py
+++ src/Assignment_4_1.py
@@ -1,1 +1,21 @@
-Test
+import sqlite3
+import datetime
+
+connector = sqlite3.connect("studenci.db")
+c = connector.cursor()
+
+c.execute('''CREATE TABLE osoby (
+    nrAlbumu INTEGER NOT NULL PRIMARY KEY,
+    imie TEXT NOT NULL,
+    nazwisko TEXT NOT NULL,
+    rok_studiow DATE timestamp,
+    kierunek TEXT,
+    srednia_ocen DOUBLE
+);''')
+
+c.execute('''CREATE TABLE kierunki (
+    idKierunku INTEGER NOT NULL PRIMARY KEY,
+    nazwa TEXT NOT NULL,
+    skrot TEXT NOT NULL,
+    otwarty BOOLEAN,
+);''')
